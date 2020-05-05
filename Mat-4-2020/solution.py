3ee8cd1cf06dfbda2e66f29c3e38e1007fc4bb6e
kevinam99 <kevinam99@gmail.com>
2020-05-05 22:13:19 +0530

    OrderedDict not the right solution

--- Mat-4-2020/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findComplement(self, num: int) -> int:
-        bin_num = bin(num)[2:]
-        ans = ''
-        for i in range(len(bin_num)):
-            if bin_num[i] == '1': ans+= '0'
-            if bin_num[i] == '0': ans+= '1'
-        
-        return int(ans, 2)
--- May-5-2020/solution.py
+++ May-5-2020/solution.py
@@ -1,3 +1,14 @@
+# can't use set since:
+#   1. it is not iterable
+#   2. it doesn't sort the in the original order
+
+from collections import OrderedDict as od
+# So use Ordered dict
 def firstUniqChar(self, s: str) -> int:
-    unique = set(str.split(""))
-    return len(unique)
+        unique = od((x,"") for x in s)
+        
+        for key, value in unique.items():
+            print(key)
+
+s = 'loveleetcode'
+firstUniqChar(s)
