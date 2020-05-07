e813c0a7671b2ff7625ee98b62c6973b9c1f0d6b
kevinam99 <kevinam99@gmail.com>
2020-05-07 20:03:07 +0530

    Solutin complete May-6-2020

--- May-6-2020/solution.py
+++ May-6-2020/solution.py
@@ -1,3 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-     
+        new_list = Counter(nums)
+        majority = len(nums) / 2 # given
+        for occurence_count in new_list:
+            if new_list[occurence_count] > majority:
+                return occurence_count
+                break
+# used the Couter method from the previous day's problem
